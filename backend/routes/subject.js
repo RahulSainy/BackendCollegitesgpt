812fb678c8447b6cc849ec6db203f2eb2238ec6e
RahulSainy <kradekidrahul@gmail.com>
2023-07-20 23:20:39 +0530

    add subject added

--- backend/routes/subject.js
+++ backend/routes/subject.js
@@ -1,77 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const subjectController = require('../controllers/subject');
-const authMiddleware = require('../middlewares/auth');
-const isAdminMiddleware = require('../middlewares/user');
+const subjectController = require("../controllers/subject");
+const authMiddleware = require("../middlewares/auth");
+const isAdminMiddleware = require("../middlewares/user");
 
 // Get all subjects of a specific semester and branch
-router.get('/semester/:semester/branch/:branch/subjects', authMiddleware,subjectController.getSubjects);
+router.get(
+  "/semester/:semester/branch/:branch/subjects",
+  authMiddleware,
+  subjectController.getSubjects
+);
 
 // Get a specific subject
-router.get('/semester/:semester/branch/:branch/subjects/:subject', authMiddleware, subjectController.getSubject);
+router.get(
+  "/semester/:semester/branch/:branch/subjects/:subject",
+  authMiddleware,
+  
+  subjectController.getSubject
+);
 
 // Create a new subject
-router.post('/semester/:semester/branch/:branch/subjects',isAdminMiddleware, authMiddleware,  subjectController.createSubject);
+router.post(
+  "/semester/:semester/branch/:branch/subjects",
+  authMiddleware,
+  isAdminMiddleware,
+  subjectController.createSubject
+);
 
 // Update a specific subject
-router.put('/semester/:semester/branch/:branch/subjects/:subject',isAdminMiddleware, authMiddleware,  subjectController.updateSubject);
+router.put(
+  "/semester/:semester/branch/:branch/subjects/:subject",
+  authMiddleware,
+  isAdminMiddleware,
+  subjectController.updateSubject
+);
 
 // Delete a specific subject
-router.delete('/semester/:semester/branch/:branch/subjects/:subject',isAdminMiddleware, authMiddleware,  subjectController.deleteSubject);
+router.delete(
+  "/semester/:semester/branch/:branch/subjects/:subject",
+  authMiddleware,
+  isAdminMiddleware,
+  subjectController.deleteSubject
+);
 
 module.exports = router;
-
-
-
-
-// const express = require('express');
-// const router = express.Router();
-// const subjectController = require('../controllers/subject');
-
-// // Get all subjects of a specific semester and branch
-// router.get('/semester/:semester/branch/:branch/subjects', subjectController.getSubjects);
-
-// // Get a specific subject
-// router.get('/semester/:semester/branch/:branch/subjects/:subject', subjectController.getSubject);
-
-// // Create a new subject
-// router.post('/semester/:semester/branch/:branch/subjects', subjectController.createSubject);
-
-// // Update a specific subject
-// router.put('/semester/:semester/branch/:branch/subjects/:subject', subjectController.updateSubject);
-
-// // Delete a specific subject
-// router.delete('/semester/:semester/branch/:branch/subjects/:subject', subjectController.deleteSubject);
-
-// module.exports = router;
-
-
-
-
-
-
-
-// const express = require('express');
-// const router = express.Router();
-
-// const subjectsController = require('../controllers/subject');
-
-// // Get all subjects
-// router.get('/', subjectsController.getAllSubjects);
-
-// // Get a specific subject
-// router.get('/semester/:semesterId/branch/:branchId/subject/:subjectName', subjectsController.getSubject);
-
-
-// // Create a new subject
-// router.post('/semester/:semesterId/branch/:branchId/subject', subjectsController.createSubject);
-
-
-// // Update a subject
-// router.put('/:semesterId/branch/:branchId/subject/:subjectId', subjectsController.updateSubject);
-
-// // Delete a subject
-// router.delete('/:semesterId/branch/:branchId/subject/:subjectId', subjectsController.deleteSubject);
-
-// module.exports = router;
-
